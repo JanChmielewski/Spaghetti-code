eb20d54bedd6c78dc04e6e49dbf4094adbfcd532
JanChmielewski <52100333+JanChmielewski@users.noreply.github.com>
2020-06-29 22:40:38 +0200

    Update TaskOne.java
    Separate by commas

--- TaskOne.java
+++ TaskOne.java
@@ -4,22 +4,31 @@ public class TaskOne {
 
     public static void list() {
 
-        System.out.println("Enter a list of numbers you would like to sort and please separate them by spaces and commas and wrtie \"done\" when u finsh");
+        System.out.println("Enter a list of numbers you would like to sort and please separate them by commas.");
         System.out.println("Example: ");
-        System.out.println("1, 2, 3, 4, ...,");
+        System.out.println("1,2,3,4,...,");
         System.out.println("Enter some numbers: ");
 
         Scanner in = new Scanner(System.in);
 
+<<<<<<< Updated upstream
         List<Double> inputs = new ArrayList<Double>();
         while (in.hasNextDouble()) {
             inputs.add(in.nextDouble());
         }
+=======
+        String line = in.nextLine();
+        List<String> numbers = Arrays.asList(line.split(","));
+        List<Integer> inputs = new ArrayList<>();
+        for (String number : numbers) {
+            inputs.add(Integer.valueOf((number)));
+        }
+
+>>>>>>> Stashed changes
         System.out.println("Unsorted list: " + inputs);
 
         Collections.sort(inputs);
         System.out.println("Sorted list: " + inputs);
-
 
         System.out.println("The lowest value is: " + inputs.get(0));
         System.out.println("The highest value is: " + inputs.get(inputs.size()-1));

78ed280a9c53a03da8741eedd5c61b59d1c61f55
JanChmielewski <52100333+JanChmielewski@users.noreply.github.com>
2020-06-30 22:08:05 +0200

    case issue
    Need to figure out how to implement a way back to menu

--- TaskOne.java
+++ TaskOne.java
@@ -4,57 +4,77 @@ public class TaskOne {
 
     public static void list() {
 
-        System.out.println("Enter a list of numbers you would like to sort and please separate them by spaces and commas and wrtie \"done\" when u finsh");
-        System.out.println("Example: ");
-        System.out.println("1, 2, 3, 4, ...,");
-        System.out.println("Enter some numbers: ");
-
         Scanner in = new Scanner(System.in);
 
-//        List<Double> inputs = new ArrayList<Double>();
+        System.out.println("Enter a list of numbers here.");
+        System.out.println("Example: ");
+        System.out.println("1,2,3,4,...,");
+        System.out.println("Enter some numbers: ");
 
-        ArrayList<String> str = new ArrayList<String>();
-        ArrayList<Integer> inputs = new ArrayList<Integer>();
-
-        while (in.hasNextLine()) {
-            String input = in.nextLine();
-            try {
-                str.add(Integer.parseInt(input.replaceAll(",")));
-            } catch (NumberFormatException nfe) {
-                System.err.println(("'" + input + "'is not a number!"));
-            }
+        String line = in.nextLine();
+        List<String> numbers = Arrays.asList(line.split(","));
+        List<Integer> inputs = new ArrayList<>();
+        for (String number : numbers) {
+            inputs.add(Integer.valueOf((number)));
         }
-
-//        for(String s: inputs) {
-//            System.out.print(s + ", ");
-//        }
 
         System.out.println("Unsorted list: " + inputs);
 
-        Collections.sort(inputs);
-        System.out.println("Sorted list: " + inputs);
+        System.out.println("Here's a list of things I can do: ");
+        System.out.println("1) Sort list    2) Show the lowest and highest value    3) Show the amount of odd and even numbers   4) Sum and compare two halves of the list");
+        int decision = in.nextInt();
+        switch (decision) {
+            case 1:
+                Collections.sort(inputs);
+                System.out.println("Sorted list: " + inputs);
+                break;
+            case 2:
+                Collections.sort(inputs);
+                System.out.println("The lowest value is: " + inputs.get(0));
+                System.out.println("The highest value is: " + inputs.get(inputs.size()-1));
+                break;
+            case 3:
+                int odd = 0, even=0;
+                for(int i = 0; i<inputs.size(); i++) {
+                    if (inputs.get(i) % 2 != 0)
+                    { odd++; }
 
+                    if (inputs.get(i) % 2 == 0)
+                    { even++; }
+                }
+                System.out.println("There are: " + odd + " odd numbers");
+                System.out.println("There are: " + even + " even numbers");
+                break;
+            case 4:
+                int firstHalf = 0, secondHalf = 0;
+                int halfList = (inputs.size()/2);
 
-        System.out.println("The lowest value is: " + inputs.get(0));
-        System.out.println("The highest value is: " + inputs.get(inputs.size()-1));
+                for (int i = 0; i<inputs.size(); i++) {
+                    if (i < halfList) {
+                        firstHalf += inputs.get(i);
+                    }
+                    else {
+                        secondHalf += inputs.get(i);
+                    }
+                }
 
-        int odd = 0;
-        for(int i = 0; i<inputs.size(); i++) {
-            if (inputs.get(i) % 2 != 0)
-            {
-                odd++;
-            }
+                System.out.println("The sum of the first half is: " + firstHalf);
+                System.out.println("The sum of the second half is: " + secondHalf);
+
+                if (halfList != (inputs.size() - halfList)) {
+                    secondHalf = secondHalf - inputs.get(inputs.size() - 1);
+                }
+
+                if (firstHalf>secondHalf) {
+                    System.out.println("The sum of the first half is grater!");
+                } else if (firstHalf<secondHalf){
+                    System.out.println("The sum of the second half is grater!");
+                } else {
+                    System.out.println("Halves are equal!");
+                }
+                break;
+            default:
+                System.out.println("Invalid selection!");
         }
-        System.out.println("There are: " + odd + " odd numbers");
-
-        int even = 0;
-
-        for(int i = 0; i<inputs.size(); i++) {
-            if (inputs.get(i) % 2 == 0)
-            {
-                even++;
-            }
-        }
-        System.out.println("There are: " + even + " even numbers");
     }
 }
